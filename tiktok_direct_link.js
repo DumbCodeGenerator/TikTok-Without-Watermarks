056af2dc44fb4fbc3109a3cbd2ae033af2156e5f
krogon500 <krogon9@gmail.com>
2019-04-06 02:11:39 +0500

    Небольшой рефакторинг и устранение конфликта jQuery

--- tiktok_direct_link.js
+++ tiktok_direct_link.js
@@ -18,6 +18,7 @@
     }else if(loc.includes('/share/video/')){
         getLink(false)
     }else if(loc.includes('m.tiktok.com/v/')){
+        jQuery.noConflict(true);
         getLink(true)
     }else{
         const pushState = history.pushState;
@@ -39,12 +40,11 @@ function loopVideo(){
 }
 
 function parseLink(document){
-    const result = document.evaluate('/html/body/script[2]', document, null, XPathResult.STRING_TYPE, null);
-    const script = result.stringValue;
+    const docText = $(document).text();
 
-    const startIndex = script.indexOf("play_addr");
-    const length = (script.lastIndexOf("},\"cover\"") + 1) - startIndex;
-    const play_addr = script.substr(startIndex, length);
+    const startIndex = docText.indexOf("play_addr");
+    const length = (docText.lastIndexOf("},\"cover\"") + 1) - startIndex;
+    const play_addr = docText.substr(startIndex, length);
 
     const uriStart = play_addr.lastIndexOf("\"uri\":\"") + 7;
     const uriLength = play_addr.lastIndexOf("\"}") - uriStart;
@@ -57,14 +57,13 @@ function getLink(isVm){
     if(!isVm){
         const url = location.href;
         const id = url.substr(url.lastIndexOf('/') + 1, 19);
-        $.ajax({ url: `https://m.tiktok.com/v/${id}.html`, success: function(data) {
-                const doc = new DOMParser().parseFromString(data, 'text/html');
-                const href = `<a class="count" style="color: #009ec8; cursor: pointer; text-decoration: underline;" href=${parseLink(doc)} target="_blank">Без вотермарки</a>`;
+        $.get(`https://m.tiktok.com/v/${id}.html`, function(data) {
+                const href = `<a class="count" style="color: #009ec8; cursor: pointer; text-decoration: underline;" href=${parseLink(data)} target="_blank">Без вотермарки</a>`;
                 $("div.count").after(href);
                 $("a.count").on('click', function() {
                     stopVideo()
                 })
-            } });
+            });
     }else{
         const href = `<a id="caption" style="color: #009ec8; cursor: pointer; text-decoration: underline;" href=${parseLink(document)} target="_blank">Без вотермарки</a>`;
         $("p#caption").after(href);
