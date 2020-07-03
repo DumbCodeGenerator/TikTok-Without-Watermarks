dacca945042277adf4bf7d66dcc90c99cd5f56f5
Kirill <krogon9@gmail.com>
2020-07-04 00:17:46 +0500

    Removed jQuery

--- tiktok_without_watermarks.user.js
+++ tiktok_without_watermarks.user.js
@@ -1,46 +1,47 @@
 // ==UserScript==
 // @name         TikTok Without Watermarks
+// @icon         https://www.google.com/s2/favicons?domain=tiktok.com
 // @homepage     https://github.com/DumbCodeGenerator/TikTok-Without-Watermarks
-// @version      0.7
+// @version      0.8
 // @description  Меняет видосы на сайте ТикТока на видосы без вотермарки
 // @author       DumbCodeGenerator
 // @run-at       document-end
 // @match        *://*.tiktok.com/*
-// @require      https://code.jquery.com/jquery-3.5.1.slim.min.js
 // @grant        GM_xmlhttpRequest
 // @connect      tiktokcdn.com
 // ==/UserScript==
 
 (function() {
     'use strict';
-    const loc = location.href;
     const query = [];
 
     const observer = new MutationObserver(function(mutations) {
-        const video = $('video.video-player.horizontal');
-        if(video.length === 0) return;
-        let link = video.attr('src');
+        const video = document.querySelector('video.video-player.horizontal');
+        if(video == null) return;
+        let link = video.src;
         if(!link.includes('/aweme/v1/play') && !query.includes(link)){
             query.push(link);
             GM_xmlhttpRequest({
                 method: 'GET',
                 url: link,
-                headers: {'Range':'bytes=12000-18000'},
+                headers: {'Range':'bytes=0-50000'},
                 onload: function (response) {
                     const regex = /vid:(.{32})/;
                     const videoId = response.responseText.match(regex)[1];
                     const noWM = `https://api2-16-h2.musical.ly/aweme/v1/play/?video_id=${videoId}&vr_type=0&is_play_url=1&source=PackSourceEnum_PUBLISH&media_type=4&ratio=default&improve_bitrate=1`;
-                    video.attr('src', noWM);
-                    video.get(0).play();
+                    video.src = noWM;
+                    if(video.paused){
+                        video.play();
+                    }
                     removeA(query, link);
                 }
             })
         }
     });
 
-    const config = { childList: true, subtree: true };
+    const config = { childList: true, subtree: true, characterData: true };
 
-    observer.observe($('#main').get(0), config);
+    observer.observe(document.getElementById('main'), config);
 })();
 
 function removeA(arr) {
